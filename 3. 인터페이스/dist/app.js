9282fe72a80d2af0b9022dea251032dd94765731
choseongchan3712 <choseongchan3712@naver.com>
2024-10-04 03:00:27 +0900

    interface

---
+++ 3. 인터페이스/dist/app.js
@@ -0,0 +1,22 @@
+"use strict";
+//% 인터페이스
+let person = {
+    name: "John",
+    age: 30
+};
+let person1 = {
+    name: "Alice"
+};
+let person2 = {
+    name: "Alice",
+    age: 25
+};
+let myCar = {
+    brand: "Toyota",
+    model: "Corolla"
+};
+let myDog = {
+    name: "Bubby",
+    breed: "Golden Retriever"
+};
+//? 위 코드에서 Dog 인터페이스는 Animal 인터페이스를 확장하여 name 속성뿐만 아니라 breed 속성도 가져야 합니다.
